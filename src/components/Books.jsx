f3f0233049de239e28f1f84ac69ffbc58e7d160a
Kenshinvag <nasro@imadrassa.com>
2021-10-23 11:03:06 +0100

    Added comments, and passing data from Book to its container.

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import SingleBook from './SingleBook'
 
 class Books extends Component {
+    // 1- Afin de faire une passation d'information entre fils(SingleBook) et parents (Books) on doit déclarer une fonction 
+    //avec comme argument l'information que tu veux récupérer depuis le fils
+    addToCart1 = (title) => {
+        // title est récupéré depuis le fils quand on click
+        console.log('Adding book', title);
+    }
+
     render(){
-        return (<div>
-            {this.props.books.map((el) => <SingleBook book={el}/>)}
+        return (<div style={{ 
+            display: 'flex',
+            flexWrap: 'wrap',
+            
+        }}>
+            {/* 2- On passe la fonction addToCart au singleBook pour récupérer l'information title a chaque fois on click sur le bouton add */}
+            {this.props.books.map((el) => <SingleBook book={el} addBook={this.addToCart1}/>)}
         </div>)
     }
 }
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import { Card } from 'antd';
+import { PlusOutlined } from '@ant-design/icons'
 
 class SingleBook extends Component {
 
     render(){
+        // 3- Ici on execute à chque fois on click la fonction passée par le parebnt, en mettant les données du titre comme argument,
+        // Le nom de la fonction est le nom du prop à traver lequel on l'a passé
         return(<Card
-                style={{ width: 300 }}
+                style={{ 
+                    width: 200,
+                    margin: 10
+                }}
                 cover={
-                <img
-                    alt="example"
-                    src={this.props.book.formats["image/jpeg"]}
-                />
+                    <img src={this.props.book.formats["image/jpeg"]} />
                 }
                 actions={[
+                    <PlusOutlined onClick={e => this.props.addBook(this.props.book.title)} />
                 ]}
             >
                 <Card.Meta
